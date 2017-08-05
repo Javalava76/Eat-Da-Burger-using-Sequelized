8b7fa00918d1bf76caeec76538d1d5d338c669d9
Kira Zoltan <kira@Kiras-MacBook-Pro.local>
2017-08-05 09:11:11 -0700

    tried to fix it

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -1,38 +1,37 @@
 var express = require("express");
 
 var router = express.Router();
-var burger = require("../models/burger.js");
+var burger = require("../models");
 
 // get route -> index
 router.get("/", function(req, res) {
-  res.redirect("/burgers");
-});
+  db.Burger.findAll({}).then(function(data){
+   var hbsObject = {
+      burger: data
+    };
 
-router.get("/burgers", function(req, res) {
-  // express callback response by calling burger.selectAllBurger
-  burger.all(function(burgerData) {
-    // wrapper for orm.js that using MySQL query callback will return burger_data, render to index with handlebar
-    res.render("index", { burger_data: burgerData });
-  });
+    res.render("index", hbsObject);
+  })
+
 });
 
 // post route -> back to index
 router.post("/burgers/create", function(req, res) {
   // takes the request object using it as input for burger.addBurger
-  burger.create(req.body.burger_name, function(result) {
+  db.Burger.create(req.body.burger_name, function(result) {
     // wrapper for orm.js that using MySQL insert callback will return a log to console,
     // render back to index with handle
-    console.log(result);
+    
     res.redirect("/");
   });
 });
 
 // put route -> back to index
 router.put("/burgers/update", function(req, res) {
-  burger.update(req.body.burger_id, function(result) {
+  db.Burger.update(req.body.burger_id, function(result) {
     // wrapper for orm.js that using MySQL update callback will return a log to console,
     // render back to index with handle
-    console.log(result);
+    
     res.redirect("/");
   });
 });
